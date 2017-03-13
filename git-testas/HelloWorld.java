8f13467fc96f8ee5c263240c6202b348c8a7d329
laurynas <laurynas,medzevepris@gmail.com>
2017-03-13 14:50:24 +0200

    added input functionality to this

--- git-testas/HelloWorld.java
+++ git-testas/HelloWorld.java
@@ -8,6 +8,10 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("HELLO UGLY WORLD!");
-		System.out.println("The World is not ugly");
+		System.out.println("THE WORLD IS NOT UGLY!");
+		Input i = new Input();
+		System.out.println("Iveskite savo varda :");
+		System.out.println("Hello "+i.input()+" !");
+		
 	}
 }
