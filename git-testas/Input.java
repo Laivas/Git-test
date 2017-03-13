a661e10c1c3f313a7907d23974320cae6f825fe6
laurynas <laurynas,medzevepris@gmail.com>
2017-03-13 14:51:15 +0200

    git-testas/Input.java
    fixed input method to return String instead of void

--- git-testas/Input.java
+++ git-testas/Input.java
@@ -2,15 +2,13 @@ import java.util.Scanner;
 
 public class Input {
 	
-	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	
+	public String input() {
 		
-		Scanner sc = new Scanner(System.in);
-		int i=0;
-		System.out.println("Iveskite skaiciu:");
-		i = sc.nextInt();
-		System.out.println("Jusu ivestas skaicius: "+i);
-		
-		
+		String a = sc.nextLine();
+		return a;		
 	}
+	
 
 }
