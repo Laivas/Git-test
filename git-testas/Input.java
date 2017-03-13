d549c4edc98f9fa92a309e83230a92d0a57da482
laurynas <laurynas,medzevepris@gmail.com>
2017-03-13 14:33:01 +0200

    new class with input functionality

---
+++ git-testas/Input.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Input {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int i=0;
+		System.out.println("Iveskite skaiciu:");
+		i = sc.nextInt();
+		System.out.println("Jusu ivestas skaicius: "+i);
+		
+		
+	}
+
+}
